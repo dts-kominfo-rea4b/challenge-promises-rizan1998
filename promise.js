c0bfa2f873ee8ca879524dbb306f918e14523022
rizan1998 <rizanalfalah@gmail.com>
2022-09-18 18:12:04 +0700

    mengerjakan promise challenge -mengubah promise.js -jalankan node index.js

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    const arrayResult = [];
+    const TheaterIXX = promiseTheaterIXX();
+    TheaterIXX.then((result) => {
+      result.forEach((element) => {
+        if (element.hasil === emosi) {
+          arrayResult.push(element.hasil);
+        }
+      });
+    });
+
+    const TheaterVGC = promiseTheaterVGC();
+    TheaterVGC.then((result) => {
+      result.forEach((element) => {
+        if (element.hasil === emosi) {
+          arrayResult.push(element.hasil);
+        }
+      });
+      resolve(arrayResult.length);
+    });
+  });
+};
 
 module.exports = {
   promiseOutput,
